0323604ee5a9b9ac70e6c13e000bbd31cb9ab8d2
xiaoxTM <gilyou.public@gmail.com>
2021-05-14 16:55:33 +0800

    Update __init__.py

--- reporters/__init__.py
+++ reporters/__init__.py
@@ -1,10 +1,3 @@
 from .logger import *
 from .mail import *
 from .progressbar import *
-from .plots import *
-
-try:
-    from .webviz import Webviz
-except Exception as e:
-    import logging
-    logging.warning(traceback.print_exc())
