f325d925540af1c5f25e30b7afc21f0c28cceb15
xiaoxTM <gilyou.public@gmail.com>
2021-06-12 10:27:28 +0800

    Update backups.py

--- ios/backups.py
+++ ios/backups.py
@@ -12,13 +12,21 @@ def backups(dst,files):
     for f in files:
         real = os.path.realpath(f)
         if os.path.exists(real):
+            dirname = os.path.dirname(f)
+            if dirname is not None and dirname != '':
+                if dirname[0] == '/':
+                    dirname = dirname[1:]
+                ddst = os.path.join(dst,dirname)
+                os.makedirs(ddst,exist_ok=True)
+            else:
+                ddst = dst
             abspath = os.path.abspath(real)
             if os.path.isfile(abspath):
-                shutil.copy2(abspath,dst)
+                shutil.copy2(abspath,ddst)
             elif os.path.isdir(abspath):
                 base = os.path.basename(f)
-                d = os.path.join(dst,base)
-                shutil.copytree(abspath,d)
+                ddst = os.path.join(ddst,base)
+                shutil.copytree(abspath,ddst)
             else:
                 raise TypeError('File or Directory: {} is neither FILE nor DIRECTORY'.format(abspath))
         else:
