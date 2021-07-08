53b3d2462e62b9a33fb3a56f6dfd045f0ddb8d1a
xiaoxTM <gilyou.public@gmail.com>
2021-07-09 04:28:31 +0800

    Delete main.py

--- utils/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-import time
-import os
-
-if __name__ == '__main__':
-    print('begin',sys.argv)
-    print(os.environ['CUDA_VISIBLE_DEVICES'])
-    time.sleep(10)
-    print('end',sys.argv)
