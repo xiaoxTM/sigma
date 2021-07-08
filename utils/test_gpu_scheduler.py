efed6fcfbb5228b3c75c70bdadffbca196e2b52d
xiaoxTM <gilyou.public@gmail.com>
2021-07-09 04:28:21 +0800

    Delete test_gpu_scheduler.py

--- utils/test_gpu_scheduler.py
+++
@@ -1,7 +0,0 @@
-import gpu_scheduler as gs
-
-tm = gs.TaskManager('test.json',poolsize=2,excludes=3)
-tm.run()
-print('task manager splitter-----------------------------------------------------------')
-tm2 = gs.TaskManager('{"command":"python3 main.py --gputest","block":true,"gpus":5,"memory":1000}',poolsize=2)
-tm2.run()
